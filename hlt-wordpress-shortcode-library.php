762324757b5c846657611be42ed958bd84a2ea37
paultgoodchild <paultgoodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-03 01:53:21 +0000

    Added: Initial Release 1.0
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-shortcode-library/trunk@470434 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hlt-wordpress-shortcode-library.php
@@ -0,0 +1,124 @@
+<?php
+/* 
+Plugin Name: WordPress Shortcode Library by Host Like Toast
+Plugin URI: http://www.hostliketoast.com/wordpress-resource-centre/
+Description: Collection of Shortcodes for Wordpress and a place for you to define your own. <a href="http://www.hostliketoast.com/2011/12/extending-wordpress-powerful-shortcodes/">See here for more information</a>.
+Version: 1.0
+Author: Host Like Toast
+Author URI: http://www.hostliketoast.com 
+*/
+
+/**
+ * Copyright (c) 2011 Host Like Toast <helpdesk@hostliketoast.com>
+ * All rights reserved.
+ * 
+ * "WordPress Shortcode Library by Host Like Toast" is distributed under the GNU General Public License, Version 2,
+ * June 1991. Copyright (C) 1989, 1991 Free Software Foundation, Inc., 51 Franklin
+ * St, Fifth Floor, Boston, MA 02110, USA
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+class HLT_WordPressShortcodeLibrary {
+	
+	public function __construct() {
+		$aMethods = get_class_methods( $this );
+		$aExclude = array( 'def', 'cleanHtml' );
+		foreach ( $aMethods as $sMethod ) {
+			if ( !in_array( $sMethod, $aExclude ) ) {
+				add_shortcode( strtoupper( $sMethod ), array( &$this, $sMethod ) );
+			}
+		}
+	}//construct
+	
+	/**
+	 * Here you can create your own Shortcode.
+	 * 
+	 * The Shortcode will be called exactly the same name as the function, but in all-caps
+	 * 
+	 * Currently the shortcode name is "MYFIRSTSHORTCODE" and if you use it as it stand, the
+	 * follow line of text will be output on your WordPress site:
+	 * 
+	 * 'Enter the HTML/Javascript that you want to appear'
+	 * 
+	 * @param $inaAtts
+	 * @param $insContent
+	 */
+	public function myFirstShortCode( $inaAtts = array(), $insContent = '' ) {
+		$sReturn = 'Enter the HTML/Javascript that you want to appear';
+
+		return $sReturn;
+
+	}//myFirstShortCode
+	
+	/**
+	 * Returns an HTML string which is a <div> containing CSS to clear:both. 
+	 * 
+	 * If you enclose any text (including other shortcodes) between the shortcode
+	 * this will be printed within the DIV.
+	 * 
+	 * This is an example of nested shortcodes.
+	 * 
+	 * @param array $inaAtts
+	 * @param string $insContent
+	 */
+	public function divClear( $inaAtts = array(), $insContent = '' ) {
+		
+		$sReturn = do_shortcode('[HTMLDIV style="clear:both" id="bob" class=""]'.$insContent.'[/HTMLDIV]');
+
+		return $sReturn;
+
+	}//divclear
+	
+	/**
+	 * A function that will output an HTML DIV element. To give the DIV classes or an ID
+	 * simply use shortcode attributes when using the shortcode in your post.
+	 * 
+	 * e.g. [HTMLDIV class="my-div-class" id="my-div-id"] div content [/HTMLDIV]
+	 * gives: <div id="my-div-id" class="my-div-class"> div content </div>
+	 * 
+	 * @param $inaAtts
+	 * @param $insContent
+	 */
+	public function htmlDiv( $inaAtts = array(), $insContent = '' ) {
+
+		$this->def( &$inaAtts, 'class' );
+		$this->def( &$inaAtts, 'id' );
+		$this->def( &$inaAtts, 'style' );
+		
+		$sReturn = '<div '.$this->cleanHtml( $inaAtts['style'], 'style' )
+					.' '.$this->cleanHtml( $inaAtts['id'], 'id' )
+					.' '.$this->cleanHtml( $inaAtts['class'], 'class' )
+					.'>'.do_shortcode( $insContent ).'</div>';
+
+		return $sReturn;
+
+	}//htmlDiv
+
+	/**
+	 * A helper function; not a WordPress Shortcode.
+	 */
+	protected function def( &$aSrc, $insKey, $insValue = '' ) {
+		if ( !isset( $aSrc[$insKey] ) ) {
+			$aSrc[$insKey] = $insValue;
+		}
+	}
+	/**
+	 * A helper function; not a WordPress Shortcode.
+	 */
+	protected function cleanHtml( $insCont, $insTag = '' ) {
+		return (($insCont != '')? ' '.$insTag.'="'.$insCont.'" ' : '' );	
+	}
+
+}//class HLT_WordPressShortcodeLibrary
+
+$oHLT_WordPressShortcodeLibrary = new HLT_WordPressShortcodeLibrary();
---
+++ readme.txt
@@ -0,0 +1,70 @@
+=== Plugin Name ===
+Contributors: paultgoodchild
+Donate link: http://www.hostliketoast.com/
+Tags: Shortcodes, Library, WordPress
+Requires at least: 3.2.0
+Tested up to: 3.3
+Stable tag: 1.0
+
+With Wordpress Shortcode Library we bring you a collection of Shortcodes to help you get more from WordPress.
+
+== Description ==
+
+Currently there are 2 active shortcodes:
+
+[DIVCLEAR]
+This will create a piece of HTML that is a DIV element with style=clear:both.
+
+[HTMLDIV]
+You can add a DIV element with parameters:
+* class=""
+* id=""
+* style=""
+
+You are free to add your own.
+
+More information can be seen here: [Host Like Toast](http://www.hostliketoast.com/2011/12/how-extend-wordpress-powerful-shortcodes/ "Host Like Toast: How To Extend WordPress With Powerful Shortcodes")
+
+== Installation ==
+
+This plugin should install as any other Wordpress.org respository plugin.
+
+1.	Browse to Plugins -> Add Plugin
+1.	Search: Wordpress Shortcode Library
+1.	Click Install
+1.	Click to Activate.
+
+Alternatively using FTP:
+
+1.	Download the zip file using the download link to the right.
+1.	Extract the contents of the file and locate the folder called 'wordpress-shortcode-library' containing the plugin files.
+1.	Upload this whole folder to your '/wp-content/plugins/' directory
+1.	From the plugins page within Wordpress locate the plugin 'Wordpress Shortcode Library' and click Activate
+
+The new Shortcodes will now be available to you.
+
+Select the CSS file as desired.
+
+== Frequently Asked Questions ==
+
+= There is no page to modify shortcodes? =
+
+We haven't built a configuration page into the plugin (yet).
+
+= How do I add Shortcodes of my own? =
+
+Go to 'Plugins' => 'Editor'
+
+On the top-right select 'Wordpress Shortcode Library' from the menu.
+
+Edit as desired.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0 =
+* Initial release.
